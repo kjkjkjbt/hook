ad66f6f3d2cb3dd61b9a8cd7c6df6470ee6b6f4e
BTGIT <lucywin2610@yahoo.com>
2024-03-18 20:06:50 +0700

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,7 @@ const Header = () => {
                 <li>
                   <NavLink
                     to="#"
-                    classname="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                    className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
                     aria-current="page"
                   >
                     Home
--- src/page/DemoHookFormik/DemoHookFormik.js
+++ src/page/DemoHookFormik/DemoHookFormik.js
@@ -1,9 +1,38 @@
 // rafce
+import { useFormik } from "formik";
 import React from "react";
+import * as yup from  "yup";
 
 const DemoHookFormik = () => {
+    const formRegister = useFormik ({
+        initialValues :{ fullName: "", phone :"" , pass : "" , email:"" },
+        onSubmit:(values) => {
+            console.log(values);
+        },
+        validationSchema:yup.object( ).shape({
+            fullName:yup
+            .string()
+            .required ("can't be blank")
+            .min (2, "too short")
+            .max (10, "more than 6 letters and less than 10 letters"),
+            email :yup 
+            .string ()
+            // .email("invalid Email")
+            .required ("Email can not be empty")
+            .matches (/^[a-zA-Z0-9._%+-]+@gmail\.com$/, 'Invalid Email'),
+            phone: yup
+            .number ()
+            .typeError("number", "Must be numbers")
+            
+        })
+    })
   return (
     <div className="container mx-auto">
+        <form
+        onSubmit={formRegister.handleSubmit}
+        action=""
+        className="border rounded p-3 space-y-4"
+      ></form>
       <form action="" className="border rounded p-3 space-y-4 ">
         <h3 className="text-3xl font-bold ">Form Register</h3>
         {/* Tên */}
@@ -14,7 +43,9 @@ const DemoHookFormik = () => {
             type="text"
             name="fullName"
             id="fullName"
+            onChange={formRegister.handleChange}
           />
+          <p className="text-red-200" > {formRegister.errors.fullName} </p>
         </div>
         {/* phone */}
         <div>
@@ -24,8 +55,12 @@ const DemoHookFormik = () => {
             type="text"
             name="phone"
             id="phone"
+            onChange={formRegister.handleChange}
           />
+        <p className="text-red-200" > {formRegister.errors.phone} </p>
+
         </div>
+
         {/* password */}
         <div>
           <label className="w-full text-xl block">insert pw </label>
@@ -34,17 +69,33 @@ const DemoHookFormik = () => {
             type="text"
             name="pass"
             id="pass"
-          />
+            onChange={formRegister.handleChange}
+          />       
+           <p className="text-red-200" > {formRegister.errors.pass} </p>
+
         </div>
+
         {/* email */}
         <div>
-          <label className="w-full text-xl block">Nhập số pass</label>
+          <label className="w-full text-xl block">Nhập email </label>
           <input
             className="w-1/2 border rounded-lg p-2"
             type="text"
             name="email"
             id="email"
+            onChange={formRegister.handleChange}
           />
+            <p className="text-red-200" > {formRegister.errors.email} </p>
+
+        </div>
+        {/* Button */}
+        <div>
+          <button
+            type="submit"
+            className="bg-green-600 text-white rounded-lg p-2"
+          >
+            Register
+          </button>
         </div>
       </form>
     </div>
